e2905a29d45f120bcd3718deb6b4d16443fe069d
祝珠红 <745729377@qq.com>
2023-04-16 20:14:11 +0800

    调整详情页面的跳转方式，实现单标签

--- src/views/Home/components/Commodity/index.vue
+++ src/views/Home/components/Commodity/index.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="wrapBox" v-if="item" @click="openItem">
-    <Pic :prop_src="item.defaultPicUrl"></Pic>
-    <!-- 商品名称 -->
-    <div>
-      <p>{{ item.name }}</p>
+    <div class="wrapBox" v-if="item" @click="openItem">
+        <Pic :prop_src="item.defaultPicUrl"></Pic>
+        <!-- 商品名称 -->
+        <div>
+            <p>{{ item.name }}</p>
+        </div>
+        <!-- 价格 -->
+        <div>￥{{ (item.defaultPrice * 1).toFixed() }}</div>
     </div>
-    <!-- 价格 -->
-    <div>￥{{ (item.defaultPrice * 1).toFixed() }}</div>
-  </div>
 </template>
 <script>
 export default {
-  props: {
-    item: {
-      default: ""
+    props: {
+        item: {
+            default: ""
+        },
+        floor: {default: 1}
     },
-    floor: { default: 1 }
-  },
-  methods: {
-    openItem() {
-      this.floor == 1 && window.open("/item/" + this.item.id);
+    methods: {
+        openItem() {
+            // this.floor === 1 && window.open("/item/" + this.item.id);
+            this.floor === 1 && this.$router.push("/item/" + this.item.id);
+        }
     }
-  }
 };
 </script>
 <style lang="less" scoped>
