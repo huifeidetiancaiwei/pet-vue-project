2380b9886a7e7555d26bda09b02f9f234cd4498d
祝珠红 <745729377@qq.com>
2023-05-19 12:33:55 +0800

    调整后台接口路径

--- vue.config.js
+++ vue.config.js
@@ -3,51 +3,59 @@
 //   transpileDependencies: true
 // })
 module.exports = {
-    lintOnSave: false,
-    /*'less-loader': {
+  lintOnSave: false,
+  /*'less-loader': {
+      lessOptions: {
+          globalVars: {
+              blackFont: "#333333",
+              themeColor: "#43B292",
+              selectColor: "#FA4278",
+              greyFont: "#999999"
+          }
+      }
+  },*/
+  css: {
+    loaderOptions: {
+      less: {
         lessOptions: {
-            globalVars: {
-                blackFont: "#333333",
-                themeColor: "#43B292",
-                selectColor: "#FA4278",
-                greyFont: "#999999"
-            }
+          globalVars: {
+            blackFont: '#333333',
+            themeColor: '#43B292',
+            selectColor: '#FA4278',
+            greyFont: '#999999'
+          }
         }
-    },*/
-    css: {
-        loaderOptions: {
-            less: {
-                lessOptions: {
-                    globalVars: {
-                        blackFont: "#333333",
-                        themeColor: "#43B292",
-                        selectColor: "#FA4278",
-                        greyFont: "#999999"
-                    }
-                }
-            }
-        }
-    },
-    devServer: {
-        proxy: {
-            '/api/ums': {
-                target: 'http://localhost:8081',
-                ws: true,
-                changeOrigin: true,
-                pathRewrite: {'^/api/ums': '/ums'}
-            },
-            '/api/pms': {
-                target: 'http://localhost:8011',
-                ws: true,
-                changeOrigin: true,
-                pathRewrite: {'^/api/pms': '/pms'}
-            },
-            '/api/oms': {
-                target: 'http://localhost:8083',
-                ws: true,
-                changeOrigin: true,
-                pathRewrite: {'^/api/oms': '/oms'}
-            }
-        }
+      }
     }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9001',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {'^/api': '/'}
+      }
+    }
+    /*proxy: {
+        '/api/ums': {
+            target: 'http://localhost:8081',
+            ws: true,
+            changeOrigin: true,
+            pathRewrite: {'^/api/ums': '/ums'}
+        },
+        '/api/pms': {
+            target: 'http://localhost:8011',
+            ws: true,
+            changeOrigin: true,
+            pathRewrite: {'^/api/pms': '/pms'}
+        },
+        '/api/oms': {
+            target: 'http://localhost:8083',
+            ws: true,
+            changeOrigin: true,
+            pathRewrite: {'^/api/oms': '/oms'}
+        }
+    }*/
+  }
 }
